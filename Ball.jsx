40587125b493d12be01b04b372ed8f45585bd28b
IrynaKonovalowa <iri.moummi@gmail.com>
2024-05-23 12:09:59 +0200

    first commit

---
+++ Ball.jsx
@@ -0,0 +1,21 @@
+﻿function Ball(props){
+    const [state, setState] = React.useState({answer: props.data.answer, classCSS: props.data.classCSS, answers: props.data.answers });
+    
+const Answer = function(){
+let randomAnswerBall = Math.floor(Math.random()*state.answers.length);
+let AnswerBall = state.answers[randomAnswerBall];
+console.log(randomAnswerBall);
+console.log(AnswerBall);
+let className = "ballOpacity";
+setState({answer: AnswerBall, classCSSDefault: className, answers: state.answers }); 
+};
+const Ask = function(){    
+    let className = "ball";
+    setState({answer: state.answer, classCSS: className, answers: state.answers});
+    };
+
+    return ( 
+        <div className={state.classCSS}>          
+        <button onMouseDown={Answer} onMouseUp={Ask}>{state.answer}</button>
+        </div>);
+    }
